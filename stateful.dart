15acae080c307ef1c4de14aa5e443b0cda3c7b2a
Karuncj <83808984+Karuncj@users.noreply.github.com>
2023-03-06 21:18:58 +0530

    stateful

---
+++ stateful.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App
+  ({super.key});
+
+  @override
+  State<App> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<App
+> {
+  String str="Hello";
+
+  @override
+  void initState(){
+    super.initState();
+  print("I was started");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        body:Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children:<Widget>[
+            Center(
+          child:Text(str),
+        ),
+        TextButton(
+          child:Text("Changed text"),
+          onPressed: (){
+          str="hi";
+          setState((){});
+      },
+      ),
+      ],
+      ),
+      ),
+    );
+  }
+}
