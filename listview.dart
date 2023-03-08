678b8556e29655d9ebec3a1e5f733508f7eaadb3
Karuncj <83808984+Karuncj@users.noreply.github.com>
2023-03-08 20:29:10 +0530

    Listview

---
+++ listview.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        body:ListView(
+          scrollDirection: Axis.vertical,
+          itemExtent: 200,
+          children:<Widget> [
+            
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+             ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+            ListTile(
+              title:Text("title")
+            ),
+
+          ]
+          ,
+        )
+        )
+          );
+  }
+}
